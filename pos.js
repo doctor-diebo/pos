61a81d1c93580255ac2b58bc2d7dbb19ca0a1a21
Diego Andrade <doctor.diebo@gmail.com>
2020-11-17 11:50:59 +0000

    add test folder and try adding shape class

--- pos.js
+++ pos.js
@@ -1,3 +1,24 @@
-export const log = ({ cock }) => {
-  console.log(cock);
+module.exports = {
+  Shape: Shape
 };
+
+class Shape {
+  constructor(shapeName) {
+    switch (shapeName) {
+      case 'circle':
+        console.log('New shape is a circle!');
+        break;
+      case 'rect':
+        console.log('New shape is a rectangle!');
+        break;
+      case 'triangle':
+        console.log('New shape is a triangle!');
+        break;
+      case 'poly':
+        console.log('New shape is a polygon!');
+        break;
+      default:
+        throw 'Invalid shape name!';
+    }
+  }
+}
---
+++ test/public/sketch.js
@@ -0,0 +1,11 @@
+const pos = require('../../pos');
+const Shape = pos.Shape;
+
+function setup() {
+  createCanvas(600, 600);
+  let myShape = new Shape('poly');
+}
+
+function draw() {
+  background(220);
+}
